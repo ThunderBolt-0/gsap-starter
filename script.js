e80f1f32803cfa886057a5665a0d917413d983c4
Thunderbolt <140913026+ThunderBolt-0@users.noreply.github.com>
2025-05-21 16:44:29 -0400

    8

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Box entrance animation with cool effects
-gsap.to(".box", {
-  scrollTrigger: {
-    trigger: ".box",
-    start: "top 80%",
-    toggleActions: "play none none reverse"
-  },
-  opacity: 1,
-  scale: 1,
-  rotate: 360,
-  duration: 1.2,
-  ease: "back.out(1.7)"
+// Hero animations
+gsap.utils.toArray(".fade-in").forEach((el, i) => {
+  gsap.to(el, {
+    opacity: 1,
+    y: 0,
+    delay: i * 0.2,
+    duration: 1,
+    ease: "power2.out"
+  });
 });
 
-// Optional: hero text fade up on page load
-gsap.from(".hero h1", {
-  y: 50,
-  opacity: 0,
-  duration: 1,
-  ease: "power3.out"
+// Scroll-triggered animations
+gsap.utils.toArray(".slide-left").forEach((el) => {
+  gsap.to(el, {
+    scrollTrigger: {
+      trigger: el,
+      start: "top 80%",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    ease: "power2.out"
+  });
+});
+
+gsap.utils.toArray(".slide-right").forEach((el) => {
+  gsap.to(el, {
+    scrollTrigger: {
+      trigger: el,
+      start: "top 80%",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    ease: "power2.out"
+  });
+});
+
+gsap.utils.toArray(".fly-up").forEach((card, i) => {
+  gsap.to(card, {
+    scrollTrigger: {
+      trigger: card,
+      start: "top 85%",
+    },
+    y: 0,
+    opacity: 1,
+    delay: i * 0.1,
+    duration: 1,
+    ease: "back.out(1.7)"
+  });
+});
+
+// Scroll progress bar
+gsap.to(".progress-bar", {
+  width: "100%",
+  scrollTrigger: {
+    scrub: 0.3
+  }
 });
