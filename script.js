a511f1faab52f3fad7cbbe5ad5617a49810ec9da
Thunderbolt <140913026+ThunderBolt-0@users.noreply.github.com>
2025-05-21 16:52:52 -0400

    10

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray(".rectangle").forEach((rect, i) => {
-  const direction = i % 2 === 0 ? -200 : 200; // alternate direction
-
-  gsap.fromTo(rect,
-    {
-      x: direction,
-      opacity: 0
+gsap.fromTo(".rectangle",
+  {
+    y: 100,
+    scale: 0.8,
+    opacity: 0
+  },
+  {
+    scrollTrigger: {
+      trigger: ".rectangle",
+      start: "top 85%",
+      toggleActions: "play none none reverse"
     },
-    {
-      scrollTrigger: {
-        trigger: rect,
-        start: "top 80%",
-        toggleActions: "play none none reverse"
-      },
-      x: 0,
-      opacity: 1,
-      duration: 1,
-      ease: "power3.out"
-    }
-  );
-});
+    y: 0,
+    scale: 1,
+    opacity: 1,
+    duration: 1,
+    ease: "power3.out",
+    stagger: 0.2
+  }
+);
