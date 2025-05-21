11be1c7050df8b0e3200db8ad6fdb456577e9ca4
Thunderbolt <140913026+ThunderBolt-0@users.noreply.github.com>
2025-05-21 16:39:34 -0400

    7

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-// --- REFACTOR 🔥
-let tl = gsap
-  .timeline({
-    defaults: {
-      duration: 1,
-    },
-  })
-  .to("h1", {
-    y: -100,
-    color: "white",
-  })
-  .to(
-    ".box1",
-    {
-      x: -100,
-      backgroundColor: "pink",
-    }
-    // Controlling positions of timeline tween
-    // "+=1"
-    // "+=2"
-    // "-=1"
-    // "-=2"
-  )
-  .to(".box2", {
-    y: 100,
-    backgroundColor: "purple",
-  })
-  .to(".box3", {
-    x: 100,
-    backgroundColor: "crimson",
-  });
+// Register the ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// Animate the box when it comes into view
+gsap.to(".box", {
+  scrollTrigger: {
+    trigger: ".box",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  },
+  y: 0,
+  opacity: 1,
+  duration: 1,
+  ease: "power2.out"
+});
