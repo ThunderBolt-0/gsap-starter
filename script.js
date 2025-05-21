2c8a54e8601870484ae3339e30a7dd928714f07f
Thunderbolt <140913026+ThunderBolt-0@users.noreply.github.com>
2025-05-21 16:47:12 -0400

    9

--- script.js
+++ script.js
@@ -1,61 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Hero animations
-gsap.utils.toArray(".fade-in").forEach((el, i) => {
-  gsap.to(el, {
-    opacity: 1,
-    y: 0,
-    delay: i * 0.2,
-    duration: 1,
-    ease: "power2.out"
-  });
-});
+gsap.utils.toArray(".rectangle").forEach((rect, i) => {
+  const direction = i % 2 === 0 ? -200 : 200; // alternate direction
 
-// Scroll-triggered animations
-gsap.utils.toArray(".slide-left").forEach((el) => {
-  gsap.to(el, {
-    scrollTrigger: {
-      trigger: el,
-      start: "top 80%",
+  gsap.fromTo(rect,
+    {
+      x: direction,
+      opacity: 0
     },
-    opacity: 1,
-    x: 0,
-    duration: 1,
-    ease: "power2.out"
-  });
-});
-
-gsap.utils.toArray(".slide-right").forEach((el) => {
-  gsap.to(el, {
-    scrollTrigger: {
-      trigger: el,
-      start: "top 80%",
-    },
-    opacity: 1,
-    x: 0,
-    duration: 1,
-    ease: "power2.out"
-  });
-});
-
-gsap.utils.toArray(".fly-up").forEach((card, i) => {
-  gsap.to(card, {
-    scrollTrigger: {
-      trigger: card,
-      start: "top 85%",
-    },
-    y: 0,
-    opacity: 1,
-    delay: i * 0.1,
-    duration: 1,
-    ease: "back.out(1.7)"
-  });
-});
-
-// Scroll progress bar
-gsap.to(".progress-bar", {
-  width: "100%",
-  scrollTrigger: {
-    scrub: 0.3
-  }
+    {
+      scrollTrigger: {
+        trigger: rect,
+        start: "top 80%",
+        toggleActions: "play none none reverse"
+      },
+      x: 0,
+      opacity: 1,
+      duration: 1,
+      ease: "power3.out"
+    }
+  );
 });
