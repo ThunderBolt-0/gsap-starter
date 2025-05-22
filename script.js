5598246665277984216a5662a2e835626471f8e2
Thunderbolt <140913026+ThunderBolt-0@users.noreply.github.com>
2025-05-22 12:00:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-gsap.registerPlugin(ScrollTrigger) 
+const string = document.getElementById("string");
+let path = `M 100 200 Q 500 200 900 200`
+let finalPath = `M 100 200 Q 500 200 900 200`
 
-gsap.to('.a', {
-    scrollTrigger: '.box', // start the animation when ".box" enters the viewport (once)
-    x: 500
-});
+string.addEventListener("mousemove", (dets) => {
+    path = `M 100 200 Q ${dets.x} ${dets.y} 900 200`
+
+    gsap.to("svg path", {
+        attr: {d: path},
+        duration: 0.5,
+        ease: "power3.out",
+    })
+})
+string.addEventListener("mouseleave", () => {
+    gsap.to("svg path", {
+        attr: {d: finalPath},
+        duration: 1.5,
+        ease: "elastic.out(1,0.2)",
+    })
+})
